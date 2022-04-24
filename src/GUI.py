d9e3e54fe626feee1723c661eb863e998f29ddb5
fr3d3r1k4 <xkmeto00@vutbr.cz>
2022-04-25 00:38:25 +0200

    resolved merge conf

--- src/GUI.py
+++ src/GUI.py
@@ -13,6 +13,10 @@
 
 import mathLibrary as mth
 
+subtraction = ('(', '', '+', '/', '-', '*')
+option = {'s': 1, 'c': 1, 't': 1, 'g': 1, 'r': 2, '^': 2, '!': 1, '*': 2, '/': 2, '+': 2, '-': 2}
+operators = ['+', '-', '*', '/', '!', '^', ')', '(', 'r', 's', 'c', 't', 'g']
+
 
 def number(value):
     if isinstance(value, int):
@@ -22,7 +26,7 @@ def number(value):
     return False
 
 
-def solving_operands2(operator, a, b):
+def solving_operands(operator, a, b):
     if operator == '*':
         return mth.multiplication(a, b)
     elif operator == '/':
@@ -35,7 +39,7 @@ def solving_operands2(operator, a, b):
         return mth.subtraction(a, b)
 
 
-def solving_operands(value):
+def solving_operands2(operator, a):
     return True
 
 
@@ -43,114 +47,81 @@ def is_numeric(value):
     return value.replace(".", "", 1).isnumeric
 
 
-def long_number(str_number):
-    number = ""
-    float_number = False
-
-    for i in range(len(str_number)):
-        if str_number[i].isdigit():
-            number += str_number[i]
-
-        # Check if it is a float number
-        elif str_number[i] == '.' and not float_number and number:
-            number += str_number[i]
-            float_number = True
-
+def long_number(value):
+    num = ""
+    long = False
+    for i in range(len(value)):
+        if value[i].isdigit():
+            num += value[i]
+        elif value[i] == '.' and not long and num:
+            num += value[i]
+            long = True
         else:
             return False
-
     return True
 
 
-def tuple_string(input_str):
-    operations = ['+', '-', '*', '/', '!', '^', ')', '(', 'r', 's', 'c', 't', 'g']
-
+def tuple_string(in_string):
+    global operators
     i = 0
-    str_number = ""
+    num_string = ""
     result = []
-
-    while i < len(input_str):
-
-        if input_str[i] in operations:
-
-            # check number and add to the result
-            if str_number and long_number(str_number):
-                if '.' in str_number:
-                    result.append(float(str_number))
+    while i < len(in_string):
+        if in_string[i] in operators:
+            if num_string and long_number(num_string):
+                if '.' in num_string:
+                    result.append(float(num_string))
                 else:
-                    result.append(int(str_number))
-
-            elif str_number and not long_number(str_number):
+                    result.append(int(num_string))
+            elif num_string and not long_number(num_string):
                 return False
-
-            str_number = ""
-
-            # append operation to the result
-            result.append(input_str[i])
-
+            num_string = ""
+            result.append(in_string[i])
         else:
-            str_number += input_str[i]
-
+            num_string += in_string[i]
         i += 1
-
-    # Check string number, convert and add to the result
-    if str_number:
-        if long_number(str_number):
-            if '.' in str_number:
-                result.append(float(str_number))
+    if num_string:
+        if long_number(num_string):
+            if '.' in num_string:
+                result.append(float(num_string))
             else:
-                result.append(int(str_number))
-
+                result.append(int(num_string))
         else:
             return False
 
     return result
 
 
-def calculate_value(expression):
-    # value: type of instruction
-    # key: number of operands
-    instructions = {'s': 1, 'c': 1, 't': 1, 'g': 1, 'r': 2, '^': 2, '!': 1, '*': 2, '/': 2, '+': 2, '-': 2}
-    before_minus = ('(', '', '+', '/', '-', '*')
-
-    for symbol, count in instructions.items():
+def calculate_value(value):
+    global option
+    global subtraction
+    for operand, count in option.items():
         i = 0
-
-        while i < len(expression):
-            if (i - 1 >= 0 and expression[i] == '-' and expression[i - 1] in before_minus) \
-                    or i == 0 and expression[i] == '-':
-                expression[i + 1] = -expression[i + 1]
-                expression.pop(i)
-
-            if expression[i] == symbol:
-
+        while i < len(value):
+            if (i - 1 >= 0 and value[i] == '-' and value[i - 1] in subtraction) \
+                    or i == 0 and value[i] == '-':
+                value[i + 1] = -value[i + 1]
+                value.pop(i)
+            if value[i] == operand:
                 if count == 1:
-                    result = solving_operands(symbol, expression[i - 1])
-
+                    result = solving_operands2(operand, value[i - 1])
                     if result is ValueError:
                         return False
-
-                    expression[i - 1] = result
-                    expression.pop(i)
-
+                    value[i - 1] = result
+                    value.pop(i)
                 elif count == 2:
-                    result = solving_operands2(symbol, expression[i - 1], expression[i + 1])
-
+                    result = solving_operands(operand, value[i - 1], value[i + 1])
                     if result is ValueError:
                         return False
-
-                    expression[i - 1] = result
-
-                    expression.pop(i)
-                    expression.pop(i)
+                    value[i - 1] = result
+                    value.pop(i)
+                    value.pop(i)
                     i -= 1
-
             i += 1
+    return value
 
-    return expression
 
-
-def brackets_one_two(value):
+def brackets_first_second(value):
     brackets = False
     first_result = []
     counter = 0
@@ -165,7 +136,7 @@ def brackets_one_two(value):
         elif value[i] == ')':
             counter -= 1
             if not counter:
-                result += brackets_one_two(first_result)
+                result += brackets_first_second(first_result)
                 first_result = []
                 brackets = False
             else:
@@ -179,17 +150,13 @@ def brackets_one_two(value):
 
 
 def solve(value):
-    # convert string to tuple
     value = tuple_string(value)
     if value is False:
         return False
-    value = brackets_one_two(value)
+    value = brackets_first_second(value)
     if value is False:
         return False
-    # Convert to int or float
     value = value[0]
-    if isinstance(value, int) == 0:
-        value = int(value)
-    elif isinstance(value, float) == 0:
+    if int(value) - float(value) == 0:
         value = int(value)
     return value
